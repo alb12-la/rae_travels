c6221d1811d2d07c562f8907d95d488e2d202516
Alberto Lerdo <alb@intentionet.com>
2019-07-23 22:40:29 -0700

    create detail view

---
+++ src/app/detail-view/detail-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-view',
+  templateUrl: './detail-view.component.html',
+  styles: []
+})
+export class DetailViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
